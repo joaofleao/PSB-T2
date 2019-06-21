0a950c96e14924f43746a478af837ce756ad047a
Joao_Leao <joao.leao@edu.pucrs.br>
2019-06-20 21:39:30 -0300

    versao 1 do createCodes sem funcionar

--- main.c
+++ main.c
@@ -5,18 +5,33 @@
 #define MAXCHAR 1000
 #define length(array) (sizeof(array)/sizeof(*(array)))
 
+typedef unsigned char byte;
+
 typedef struct Node {//////////////////////////////////////////////////////////////////////////////////////////define nodo
     int frequencia;
     char txt;
     struct Node *esquerda;
     struct Node *direita;
-    struct Node *pai;
+    struct Node *father;
+    char bit;
+    bool marked;
 } Node;
 
 typedef struct NodeList{///////////////////////////////////////////////////////////////////////////////////////define nodo de lista
     Node *n;
     struct NodeList *proximo;
 } NodeList;
+
+typedef struct Word{///////////////////////////////////////////////////////////////////////////////////////define palavras de dicionario
+    byte cod;
+    struct Word *proximo;
+    char meaning;
+} Word;
+
+typedef struct Dictionary{///////////////////////////////////////////////////////////////////////////////////////define o dicionario a ser usado
+    Word *head;
+    int elementos;
+} Dictionary;
 
 typedef struct List {//////////////////////////////////////////////////////////////////////////////////////////define lista
     NodeList *head;
@@ -27,22 +42,73 @@ int main(){/////////////////////////////////////////////////////////////////////
     char *printList(List *lista);
     char *printTree(Node *novo);
     List *createList();
+    Dictionary *createCodes(List *lista, int number);
+
     printf("\nRunning\n\n");
 
     List *lista = createList();
     readiFileAndCount(lista);
 
     printf("List Size: %d\nList Content: %s\n", lista->elementos, printList(lista));
-
+    int number = lista->elementos;
     createTree(lista);
+    createCodes(lista, number);
    
     printf("List Size: %d\nList Content: %s\n", lista->elementos, printList(lista));
 
     printf("\nDone\n\n");
     return 1;  
 } 
+Dictionary *createCodes(List *lista, int number) {////////////////////////////////////////////////////transforma a arvore em uma lista de codigos, retorna lista de codigos 
+    char *printDictionary(Dictionary *dicionario);
+    char *printWord(Word *word);
+    Dictionary *d;
+    if ( ( d = malloc(sizeof(*d)) ) == NULL ) return NULL;
+    Word *w;
+    if ( ( w = malloc(sizeof(*w)) ) == NULL ) return NULL;
+    Node *x;
+    if ( ( x = malloc(sizeof(*x)) ) == NULL ) return NULL;
+    d->head = w;
+    d->elementos = 0;
 
-void createTree(List *lista) {
+    //printf("teste0: %s\n", txt);
+    while (!lista->head->n->direita->marked){
+        x = lista->head->n;
+        char *txt = (char *) malloc(sizeof(char) * 100);
+        int test = 0;
+        txt[0] = '\0';
+
+        while(x->esquerda) {
+            x = x->esquerda;
+            sprintf(txt, "%s%s", txt, "0");
+            test++;
+        }
+        w->meaning = x->txt;
+        w->cod = txt;
+        printf("txt%s", txt);
+        w = w->proximo;
+        txt[test-1] = '\0';
+        d->elementos++;
+        x->marked = true;
+        x = x->father;
+        
+        if (x->esquerda->marked && x->direita->marked) {
+            x = x->father;
+            x->marked = true;
+        } 
+        x = x->direita;
+        sprintf(txt, "%s%s", txt, "1");
+        test++;
+     printf("head%s", printWord(d->head));
+
+    }
+
+    printf("Dicionário:%s", printDictionary(d));
+
+    return d;
+}
+
+void createTree(List *lista) {////////////////////////////////////////////////////cria a arvore e acaba com a lista com somente um item (raiz)
     NodeList *menor(List *lista);
     Node *createNode(int frequencia, char txt, Node *esquerda, Node *direita);
     NodeList *createNodeList (Node *obj);
@@ -51,11 +117,14 @@ void createTree(List *lista) {
         Node *minor = menor(lista)->n;
         Node *major = menor(lista)->n;
         Node *tree = createNode(minor->frequencia+major->frequencia, '.', major, minor);
-        insereLista(createNodeList(tree), lista);
+        minor->father = tree;
+        major->father = tree;
+        major->bit = '0';
+        minor->bit = '1';
+        
     }
-
 }
-NodeList *menor(List *lista) {
+NodeList *menor(List *lista) {////////////////////////////////////////////////////////////////////////////////////popa o menor numero da lista 
     NodeList *menor = lista->head;
     lista->head = lista->head->proximo;
     lista->elementos--;
@@ -81,8 +150,8 @@ void readiFileAndCount(List *lista) {///////////////////////////////////////////
     for (i = 0; (i < 256); i++) {
         if (counts[i]!=0) {
             char c = i;
+            //printf("%c - %d\n", c, counts[i]);
             insereLista(createNodeList(createNode(counts[i], c, NULL, NULL)), lista);
-          //  printf("%c - %d\n", c, counts[i]);
         }
     }
 }
@@ -135,18 +204,40 @@ Node *createNode(int frequencia, char txt, Node *esquerda, Node *direita) {/////
     novo->txt = txt;
     novo->esquerda = esquerda;
     novo->direita = direita;
+    novo->marked = false;
     return novo;
 }
 
+char *printWord(Word *word) {//////////////////////////////////////////////////////////////////////////////////////////printa palavra
+    char *print = (char *) malloc(sizeof(char) * 1000000);
+    
+    sprintf(print, "{%c = %s}", word->meaning, word->cod);
+    
+    return print;
+}
+
+char *printDictionary(Dictionary *dicionario) {//////////////////////////////////////////////////////////////////////////////////////////printa dicionario
+    char *printWord(Word *word);
+    char *print = (char *) malloc(sizeof(char) * 1000000);
+    print[0] = '\0';
+    Word *item = dicionario->head;
+    if (!item) return "Lista Vazia";
+    for (int i = 0; item; i++) {
+        sprintf(print, "%s{%s}, ", print, printWord(item));
+        item = item->proximo;
+    }
+    return print;
+}
+
 char *printTree(Node *node) {//////////////////////////////////////////////////////////////////////////////////////////printa nodo
-    char *print = (char *) malloc(sizeof(char) * 100);
+    char *print = (char *) malloc(sizeof(char) * 1000000);
     
     sprintf(print, "%d, %c, {%d, %c}< >{%d, %c}", node->frequencia, node->txt, node->esquerda->frequencia, node->esquerda->txt, node->direita->frequencia, node->direita->txt);
     
     return print;
 }
 char *printNode(Node *node) {//////////////////////////////////////////////////////////////////////////////////////////printa nodo
-    char *print = (char *) malloc(sizeof(char) * 100);
+    char *print = (char *) malloc(sizeof(char) * 1000000);
 
     sprintf(print, "%d, %c", node->frequencia, node->txt);
 
@@ -154,7 +245,7 @@ char *printNode(Node *node) {///////////////////////////////////////////////////
 }
 
 char *printList(List *lista) {//////////////////////////////////////////////////////////////////////////////////////////printa nodo
-    char *print = (char *) malloc(sizeof(char) * 100);
+    char *print = (char *) malloc(sizeof(char) * 1000000);
     print[0] = '\0';
     NodeList *item = lista->head;
     if (!item) return "Lista Vazia";
